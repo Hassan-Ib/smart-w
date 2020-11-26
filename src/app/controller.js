cef10561912fc1d14e7bfbc865d715cecf618a7e
Hassan-Ib <azanebrahim@gmail.com>
2020-11-26 08:00:49 -0500

    added new touvh

--- src/app/controller.js
+++ src/app/controller.js
@@ -2,14 +2,19 @@ let burger = document.querySelector(".burger");
 let navLinks = document.querySelector(".navigation__list");
 let playBth = document.querySelector(".btn__play-pause");
 let video = document.querySelector(".hero__video");
+function init() {
+  video.pause();
+  burger.addEventListener("click", () => {
+    burger.classList.toggle("active");
+    navLinks.classList.toggle("toggle");
+  });
 
-video.pause();
-burger.addEventListener("click", () => {
-  burger.classList.toggle("active");
-  navLinks.classList.toggle("toggle");
-});
+  playBth.addEventListener("click", () => {
+    video.currentTime = 0;
+    // if (!video.paused) video.pause();
+    // else
+    video.play();
+  });
+}
 
-playBth.addEventListener("click", () => {
-  if (!video.paused) video.pause();
-  else video.play();
-});
+init();
